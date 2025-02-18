7da069bb8825cd88d6fe64b43e7656664632e0ef
KevinHidalgo <kevin.andres2009@hotmail.com>
2025-02-17 23:47:41 -0600

    Cambios navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // 🔹 Importa FormsModule
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   isMobile: boolean = false;
- 
+  menuOpen: boolean=false;
+  menu_icon: string='fas fa-bars';
+
   constructor() {
     this.checkScreenWidth();
+
     window.addEventListener('resize', () => this.checkScreenWidth());
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    
+    if(this.menuOpen){
+      this.menu_icon='fas fa-times';
+    }else{
+      this.menu_icon='fas fa-bars';
+    }
+    console.log(this.menu_icon);
   }
 
   checkScreenWidth() {
